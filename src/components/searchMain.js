1e012f9bdec0466b20cb6d104f76095b0e136f35
Emir Mustafoski <emirmustafoski92@gmail.com>
2023-01-06 14:41:39 +0100

    https

--- src/components/searchMain.js
+++ src/components/searchMain.js
@@ -8,7 +8,7 @@ const SearchMain = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=fd4986fb03e759b729362105d250bf33`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=fd4986fb03e759b729362105d250bf33`;
       let res = await fetch(url);
       let data = await res.json();
 
